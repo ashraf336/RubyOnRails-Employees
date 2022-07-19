d94f5b7fc58bd7f536480eb731395584e51cab70
Ahmed <ahmed99ashraf@gmail.com>
2022-07-19 14:48:00 +0200

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def about
+    @description = "House of Development & Business Creativity, HDBC, is a technology company utilizes digital innovations to improve businesses and communities. HDBC is a software house with business consultation power that helps business leaders and entrepreneurs to realize their ideas and plans. HDBC has established with a solid science, engineering and business background. We have no borders in using (and mixing) different new technologies to find out a useful innovation that impact global community."
+  end
+end
